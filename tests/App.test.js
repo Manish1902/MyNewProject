6e14fd118345303640ee66bbf87d41652f687154
Manish Singh Talan <manishtalan19@gmail.com>
2024-11-10 15:48:34 +0530

    .

--- tests/App.test.js
+++ tests/App.test.js
@@ -5,6 +5,6 @@ import App from '../App';
 describe('<App />', () => {
   it('renders correctly', () => {
     const { getByText } = render(<App />);
-    expect(getByText('Open up App.js to start working on your app!')).toBeTruthy();
+    expect(getByText('Hello, World!')).toBeTruthy();
   });
 });
